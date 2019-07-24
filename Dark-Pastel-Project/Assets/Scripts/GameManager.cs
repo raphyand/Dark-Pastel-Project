2eee80758b8daa9a00cdc1d9a8d58ef5b85b40c4
raphyand <raphyand@gmail.com>
2019-07-23 20:59:49 -0700

    Very Early Build with working WorldSwap
    World Swap works, but right now the characters are two separate players, two separate sprites, controllers, etc.

---
+++ Dark-Pastel-Project/Assets/Scripts/GameManager.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+     // Start is called before the first frame update
+
+     [SerializeField] private GameObject pastelWorldHolder, darkWorldHolder;
+     [SerializeField] private Player myPlayer;
+
+    void Start()
+    {
+          if (pastelWorldHolder.activeSelf == true)
+               darkWorldHolder.SetActive(false);
+          else
+          {
+               pastelWorldHolder.SetActive(true);
+               darkWorldHolder.SetActive(false);
+                   
+          }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+          WorldSwitch();
+    }
+
+     private void WorldSwitch()
+     {
+
+
+
+          if (Input.GetKeyDown(KeyCode.Tab) == true)
+          {
+
+               //myPlayer.SwitchBetweenDarkAndPastel();
+
+
+               Debug.Log("Tab is detected and function is called.");
+
+               if (pastelWorldHolder.activeSelf == true)
+               {
+                    darkWorldHolder.SetActive(true);
+                    pastelWorldHolder.SetActive(false);
+
+               }
+
+               else if (darkWorldHolder.activeSelf == true)
+               {
+                    darkWorldHolder.SetActive(false);
+                    pastelWorldHolder.SetActive(true);
+               }
+               else
+               {
+                    darkWorldHolder.SetActive(true);
+
+               }
+
+
+               //pastelWorldHolder.activeSelf == true ? darkWorldHolder.SetActive(true) : pastelWorldHolder.SetActive(true);//(darkWorldHolder.SetActive(true) && pastelWorldHolder.SetActive(false)) : 
+
+
+          }
+     }
+
+}
---
+++ Dark-Pastel-Project/Assets/Scripts/Player.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+     [SerializeField] float moveSpeed = 10f;
+     [SerializeField] float jumpSpeed = 5f;
+     SpriteRenderer mySpriteRenderer;
+     [SerializeField] Sprite pastel, dark;
+     Animator myAnimator;
+     [SerializeField] AnimatorControllerParameter pastelAnimator, darkAnimator;
+     
+
+     Rigidbody2D myRigidbody2D;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+          myRigidbody2D = GetComponent<Rigidbody2D>();
+          mySpriteRenderer = GetComponent<SpriteRenderer>();
+          myAnimator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+          MoveLeftRight();
+          Jump();
+          FlipSprite();
+          SwitchBetweenDarkAndPastel();
+    }
+
+     private void MoveLeftRight()
+     {
+          float deltaX = Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime;
+          var newXPos = myRigidbody2D.position.x + deltaX;
+          myRigidbody2D.velocity = new Vector2(moveSpeed * deltaX, myRigidbody2D.velocity.y);
+     }
+
+     private void Jump()
+     {
+          if (Input.GetKeyDown(KeyCode.Space) == true){
+
+               Vector2 jumpVelocityToAdd = new Vector2(0f, jumpSpeed);
+               myRigidbody2D.velocity += jumpVelocityToAdd;
+          }
+     }
+
+     private void FlipSprite()
+     {
+          bool playerHasHorizontalSpeed = Mathf.Abs(/*transform.position.x*/myRigidbody2D.velocity.x) > Mathf.Epsilon;
+          if (playerHasHorizontalSpeed)
+               transform.localScale = new Vector2(Mathf.Sign(myRigidbody2D.velocity.x), 1f);
+     }
+
+
+     public void SwitchBetweenDarkAndPastel()
+     {
+          if (mySpriteRenderer.sprite == pastel)
+          {
+               mySpriteRenderer.sprite = dark;
+          }
+          else if (mySpriteRenderer.sprite == dark)
+               mySpriteRenderer.sprite = pastel;
+          else
+               mySpriteRenderer.sprite = pastel;
+
+
+
+     }
+
+
+}
